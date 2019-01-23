a86e7111f62a4ff1193e2b4fcb6f755470c953ad
Reginald Augustin <Reginaagst@gmail.com>
2019-01-23 18:52:28 -0500

    fix 3

--- foodtasker/settings.py
+++ foodtasker/settings.py
@@ -126,7 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 LOGIN_REDIRECT_URL = '/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = '/media/' 
 
 import dj_database_url
 db_from_env = dj_database_url.config()
